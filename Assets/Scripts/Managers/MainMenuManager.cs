a38c844dcc3a5ac874c2e61774867397581f0da5
Spectrum <48571227+Spectrum-Programmer@users.noreply.github.com>
2026-04-02 21:01:38 -0700

    Unity Project

---
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,27 @@
+using TriInspector;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+    [HideMonoScript]
+    public class MainMenuManager : MonoBehaviour
+    {
+
+        [Scene] [SerializeField] private string startScene;
+
+        private static void ChangeScene(string sceneName) { SceneManager.LoadScene(sceneName, LoadSceneMode.Single); }
+
+        public void StartGame() { ChangeScene(startScene); }
+
+        public void QuitGame()
+        {
+            #if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+}
